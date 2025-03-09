bcf7d8f0e149c960e9f8ee0aba80b23e45d09f0b
2Shad <shadman.ahmed098@gmail.com>
2025-03-09 16:07:34 +0000

    added second solution to reverse_sentence

--- reverse_sentence.py
+++ reverse_sentence.py
@@ -1,7 +1,13 @@
 sentence = "Python is awsome."
 #####################
-reverse = sentence.split()
-reverse.reverse()
-reverse[0] = reverse[0][0:len(reverse[0])-1].capitalize()
-reverse[-1] = (reverse[-1] + '.').lower()
+# reverse = sentence.split()
+# reverse.reverse()
+# reverse[0] = reverse[0][0:len(reverse[0])-1].capitalize()
+# reverse[-1] = (reverse[-1] + '.').lower()
+# print(' '.join(reverse))
+
+##################################################################################################
+reverse = sentence.split()[::-1]
+reverse[0] = reverse[0][:len(reverse[0])-1].capitalize()
+reverse[-1] = reverse[-1].lower() + '.'
 print(' '.join(reverse))
